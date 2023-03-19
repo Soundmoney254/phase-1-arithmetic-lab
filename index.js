2780a3e5af1aff26141f0ab929c76c461ce5a4ad
Samuel <samnmbuguah@gmail.com>
2023-03-19 13:59:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1;
+let num2;
 
-// Write your code here
+do {
+  num1 = Math.floor(Math.random() * 10) + 1;
+  num2 = Math.floor(62 / num1);
+} while (num1 * num2 !== 62);
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 46;
+const num4 = 7;
+
+let mod = num3 % num4;
+
+
+let max = Math.max(5, 10, 15, 20);
